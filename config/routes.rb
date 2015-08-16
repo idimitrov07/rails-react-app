508736268e2186fe56016962e7838b50a9251c57
Ivan Dimitrov <idimitrov07@gmail.com>
2015-08-16 19:39:23 +0300

    Move delete button to the right.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :records
-  root to: 'pages#index'
+  root to: 'records#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
